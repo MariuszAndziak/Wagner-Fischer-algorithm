d8055ffd1e51bc7c566e966e3715d3d81324212c
MariuszAndziak <43908826+MariuszAndziak@users.noreply.github.com>
2024-03-06 20:21:39 +0100

    Working alpha version

--- sketch.js
+++ sketch.js
@@ -22,14 +22,27 @@ function run() {
   const gridSizeY = word2.length; // Number of rows
   const squareSize = min(width / gridSizeX/2, height / gridSizeY/2);
   
-  // Draw squares for each letter in word1
-  for (let i = 0; i <= word1.length; i++) {
-    for (let j = 0; j <= word2.length; j++) {
-      const x = i * squareSize;
-      const y = j * squareSize;
-      drawSquare(x, y, squareSize, '0'); // Fill all squares with '_'
+  
+  
+  const distances = [];
+  for (let i = 0; i < word2.length; i++) {
+    distances.push([]);
+    for (let j = 0; j < word1.length; j++) {
+      const distance = wagner_fischer(word1.slice(0, j + 1), word2.slice(0, i + 1));
+      distances[i].push(distance);
     }
   }
+  
+  
+  for (let i = 0; i < word1.length; i++) {
+    for (let j = 0; j < word2.length; j++) {
+      const x = (i + 1) * squareSize; // Offset by one position to the right
+      const y = (j + 1) * squareSize; // Offset by one position down
+      drawSquare(x, y, squareSize, distances[j][i]);
+    }
+  }
+
+
   
   // Draw squares for each letter in word1 (first row)
   for (let i = 0; i < word1.length; i++) {
@@ -51,4 +64,31 @@ function drawSquare(x, y, size, letter) {
   textSize(20);
   textAlign(CENTER, CENTER);
   text(letter, x + size / 2, y + size / 2);
+}
+
+
+
+function wagner_fischer(s1, s2) {
+  let len_s1 = s1.length;
+  let len_s2 = s2.length;
+  if (len_s1 > len_s2) {
+    [s1, s2] = [s2, s1];
+    [len_s1, len_s2] = [len_s2, len_s1];
+  }
+
+  let curr_row = Array.from({length: len_s1 + 1}, (_, i) => i);
+  for (let i = 1; i <= len_s2; i++) {
+    let prev_row = curr_row.slice();
+    curr_row[0] = i;
+    for (let j = 1; j <= len_s1; j++) {
+      let add = prev_row[j] + 1;
+      let del = curr_row[j - 1] + 1;
+      let change = prev_row[j - 1];
+      if (s1[j - 1] != s2[i - 1]) {
+        change += 1;
+      }
+      curr_row[j] = Math.min(add, del, change);
+    }
+  }
+  return curr_row[len_s1];
 }
